3ff14fd940d628e9fb292344c2077457c46d7759
Parvesh Yadav <py3073800@gmail.com>
2026-07-22 05:18:45 +0530

    done

---
+++ coding files/program8.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+
+    cout<<"enter value of n"<<endl;
+    cin>>n;
+
+int i=1;
+i <= n;
+
+int j = 1;
+
+    while(j<= n){
+
+        cout<<'A'+i-1;
+
+        j =j+1;
+    }
+
+
+
+return 0;
+}
