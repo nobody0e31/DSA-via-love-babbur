4989e29a1515f2503ec524887f33454575658546
Parvesh Yadav <py3073800@gmail.com>
2026-07-22 07:37:34 +0530

    done

--- coding files/program8.cpp
+++ coding files/program8.cpp
@@ -9,13 +9,14 @@ int main(){
     cin>>n;
 
 int row = 1;
+ while(row <= n){
 
     int col = 1;
-        while(col <= n){
-                    char ch = 'A'+row-1;
-                    cout<< ch ;
+    while(col <= n){
+    char ch = 'A'+row-1;
+    cout<< ch ;
 
-        col+=1;
+    col+=1;
 
         
                 }
